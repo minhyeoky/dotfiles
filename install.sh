16b88bfe08e0bcb8741fd1464c3564b4bb113913
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-04-06 04:03:42 +0900

    To neovim - Fix install.sh (.config/nvim)

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 #
 # Install dotfiles for Mac OSX.
 
-
 set -e
 
-cd $(dirname "$0")
+cd "$(dirname "$0")"
 source ./utils.sh
 
 guard_os "Mac"
@@ -32,12 +31,12 @@ function install_zsh() {
   ln -s -f -v "$PWD"/zsh/.p10k.zsh ~/.p10k.zsh
 
   if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
     echo "Clone zsh-syntax-highlighting done"
   fi
 
   if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
     echo "Clone zsh-autosuggestions done"
   fi
 
@@ -47,7 +46,7 @@ function install_zsh() {
   fi
 
   if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z ]]; then
-    git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+    git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z"
     echo "Clone zsh-z done"
   fi
 }
@@ -57,30 +56,20 @@ function install_vim() {
   brew update
   brew install neovim --HEAD
 
-  # Link .vimrc.
-  backup ~/.vimrc
-  ln -s -f -v "$PWD"/vim/.vimrc ~/.vimrc
-
   # Link .vim files. (Plugins)
   mkdir -p -v ~/.vim
   backup ~/.vim/.vimwiki.vim
   ln -s -f -v "$PWD"/vim/.vimwiki.vim ~/.vim/.vimwiki.vim
-  backup ~/.vim/.coc_nvim.vim
-  ln -s -f -v "$PWD"/vim/.coc_nvim.vim ~/.vim/.coc_nvim.vim
 
   # Link init.vim (neovim)
-  mkdir -p -v ~/.config/nvim
-  backup ~/.config/nvim/init.vim
-  ln -s -f -v "$PWD"/nvim/init.vim ~/.config/nvim/init.vim
-  
-  # Link nvim files. (Plugins)
-  backup ~/.config/nvim/coc-settings.json
-  ln -s -f -v "$PWD"/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+  mkdir -p -v ~/.config
+  backup ~/.config/nvim
+  ln -s -f -v "${PWD}/nvim" ~/.config
 
   # Install vim-plug.
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  
+
   # Install ripgrep, fd.
   brew install ripgrep
   brew install fd
@@ -94,23 +83,22 @@ function install_git() {
   ln -s -f -v "$PWD"/git/.gitignore ~/.gitignore
 }
 
-read -p "---- Install bash? [Y/n]: " input
+read -r -p "---- Install bash? [Y/n]: " input
 if [[ ${input} != "n" ]]; then
   install_bash
 fi
 
-read -p "---- Install zsh? [Y/n]: " input
+read -r -p "---- Install zsh? [Y/n]: " input
 if [[ ${input} != "n" ]]; then
   install_zsh
 fi
 
-read -p "---- Install vim? [Y/n]: " input
+read -r -p "---- Install vim? [Y/n]: " input
 if [[ ${input} != "n" ]]; then
   install_vim
 fi
 
-read -p "---- Install git? [Y/n]: " input
+read -r -p "---- Install git? [Y/n]: " input
 if [[ ${input} != "n" ]]; then
   install_git
 fi
-
