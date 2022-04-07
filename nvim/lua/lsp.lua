e64693ccf095073f197300d017ee29abf4a1437f
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-04-07 22:42:20 +0900

    Fix miner

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,5 +1,7 @@
+-------------------------------------------------------------------------------
 -- Setup nvim-cmp.
-local cmp = require'cmp'
+-------------------------------------------------------------------------------
+local cmp = require("cmp")
 
 cmp.setup({
   snippet = {
@@ -58,7 +60,9 @@ cmp.setup.cmdline(':', {
   })
 })
 
--- Setup nvim_lsp
+-------------------------------------------------------------------------------
+-- Setup nvim-lsp.
+-------------------------------------------------------------------------------
 -- Configurations: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
 local function config(_config)
@@ -68,15 +72,7 @@ local function config(_config)
 end
 
 -- npm install -g pyright
-require('lspconfig').pyright.setup(config({
-  settings = {
-    python = {
-      analysis = {
-        diagnosticMode = "workspace",
-      }
-    }
-  }
-}))
+require("lspconfig").pyright.setup(config())
 
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run
 -- brew install lua-language-server
@@ -90,9 +86,16 @@ require("lspconfig").sumneko_lua.setup(config({
       diagnostics = {
         -- Get the language server to recognize the `vim` global
         globals = { "vim" },
-      }
-    }
-  }
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
 }))
 
 -- https://github.com/hrsh7th/vscode-langservers-extracted
@@ -102,3 +105,49 @@ require("lspconfig").jsonls.setup(config())
 -- https://github.com/mads-hartmann/bash-language-server
 -- npm install -g bash-language-server
 require("lspconfig").bashls.setup(config())
+
+-------------------------------------------------------------------------------
+-- Mappings.
+-------------------------------------------------------------------------------
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(_, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '"', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'pyright', 'rust_analyzer', 'tsserver' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
+end
