e004e6bfe1fe7b15790232805786c87e6de57362
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-09-11 16:07:52 +0900

    nvim: Run format before saving file

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -59,6 +59,13 @@ require("null-ls").setup({
 	},
 })
 
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*"},
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end
+})
+
 require("zk").setup({
 	picker = "fzf",
 })
