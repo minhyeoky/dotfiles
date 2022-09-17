3b8986a7e172868ae698ffab8b18f4331bd70ac3
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-09-17 17:33:56 +0900

    nvim: Update init.lua

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,73 +1,73 @@
 require("tokyonight").setup({
-	style = "night",
-	transparent = true,
-	dim_inactive = true,
-	lualine_bold = true,
+  style = "night",
+  transparent = true,
+  dim_inactive = true,
+  lualine_bold = true,
 })
 vim.api.nvim_cmd({
-	cmd = "colorscheme",
-	args = { "tokyonight" },
+  cmd = "colorscheme",
+  args = { "tokyonight" },
 }, {})
 
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"pyright",
-		"bashls",
-		"jsonls",
-		"sumneko_lua",
-		"flutter-tools",
-		"html",
-		"jdtls",
-		"tsserver",
-		"dockerls",
-		"cssls",
-	},
-	automatic_installation = true,
+  ensure_installed = {
+    "pyright",
+    "bashls",
+    "jsonls",
+    "sumneko_lua",
+    "flutter-tools",
+    "html",
+    "jdtls",
+    "tsserver",
+    "dockerls",
+    "cssls",
+  },
+  automatic_installation = true,
 })
 
 require("null-ls").setup({
-	sources = {
-		require("null-ls").builtins.diagnostics.actionlint,
-		require("null-ls").builtins.diagnostics.alex,
-		require("null-ls").builtins.diagnostics.codespell,
-		require("null-ls").builtins.diagnostics.pylint,
-		require("null-ls").builtins.diagnostics.shellcheck,
-		require("null-ls").builtins.diagnostics.eslint,
-		require("null-ls").builtins.diagnostics.gitlint,
-		require("null-ls").builtins.diagnostics.hadolint,
-		require("null-ls").builtins.diagnostics.jsonlint,
-		require("null-ls").builtins.diagnostics.misspell,
+  sources = {
+    require("null-ls").builtins.diagnostics.actionlint,
+    require("null-ls").builtins.diagnostics.alex,
+    require("null-ls").builtins.diagnostics.codespell,
+    -- require("null-ls").builtins.diagnostics.pylint,
+    require("null-ls").builtins.diagnostics.shellcheck,
+    require("null-ls").builtins.diagnostics.eslint,
+    require("null-ls").builtins.diagnostics.gitlint,
+    require("null-ls").builtins.diagnostics.hadolint,
+    require("null-ls").builtins.diagnostics.jsonlint,
+    require("null-ls").builtins.diagnostics.misspell,
 
-		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.black,
-		require("null-ls").builtins.formatting.isort,
-		require("null-ls").builtins.formatting.jq,
-		require("null-ls").builtins.formatting.shfmt,
-		require("null-ls").builtins.formatting.yamlfmt,
-		require("null-ls").builtins.formatting.prettier,
-		require("null-ls").builtins.formatting.cbfmt,
-		require("null-ls").builtins.formatting.trim_newlines,
-		require("null-ls").builtins.formatting.trim_whitespace,
+    require("null-ls").builtins.formatting.stylua,
+    require("null-ls").builtins.formatting.black,
+    require("null-ls").builtins.formatting.isort,
+    require("null-ls").builtins.formatting.jq,
+    require("null-ls").builtins.formatting.shfmt,
+    require("null-ls").builtins.formatting.yamlfmt,
+    require("null-ls").builtins.formatting.prettier,
+    require("null-ls").builtins.formatting.cbfmt,
+    require("null-ls").builtins.formatting.trim_newlines,
+    require("null-ls").builtins.formatting.trim_whitespace,
 
-		require("null-ls").builtins.code_actions.gitsigns,
-		require("null-ls").builtins.code_actions.shellcheck,
-		require("null-ls").builtins.code_actions.proselint,
-		require("null-ls").builtins.code_actions.refactoring,
-		require("null-ls").builtins.completion.tags,
-	},
+    require("null-ls").builtins.code_actions.gitsigns,
+    require("null-ls").builtins.code_actions.shellcheck,
+    require("null-ls").builtins.code_actions.proselint,
+    require("null-ls").builtins.code_actions.refactoring,
+    require("null-ls").builtins.completion.tags,
+  },
 })
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = {"*"},
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--   pattern = { "*" },
+--   callback = function()
+--     vim.lsp.buf.format({ async = false })
+--   end,
+-- })
 
 require("zk").setup({
-	picker = "fzf",
+  picker = "fzf",
 })
 local opts = { noremap = true, silent = false }
 
