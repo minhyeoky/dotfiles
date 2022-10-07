8b954fb72120ee1bd9e2d881170a9295c5818230
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-10-08 00:43:45 +0900

    Refactor taskzk.lua

--- nvim/lua/plugins/taskzk.lua
+++ nvim/lua/plugins/taskzk.lua
@@ -1,33 +1,38 @@
 local Path = require("plenary.path")
 local Job = require("plenary.job")
 
-local function link()
-  local cur_buf_filename = vim.api.nvim_buf_get_name(0)
-  local p = Path.new(cur_buf_filename)
+local function _is_note()
+  return Path.new(vim.api.nvim_buf_get_name(0)):parents()[1] == os.getenv("ZK_NOTEBOOK_DIR")
+end
 
-  -- Check if current file is in ZK dir or not
-  if p:parents()[1] == os.getenv("ZK_NOTEBOOK_DIR") then
-    local job = Job:new({
-      command = "task",
-      args = { "status:pending", "export" },
-    })
-    job:sync()
-    local entries = {}
-    for _, value in ipairs(vim.json.decode(vim.fn.join(job:result()))) do
-      -- Use fzf to select task
-      table.insert(entries, "[" .. value.id .. "]" .. " " .. value.description)
-    end
-    vim.fn["fzf#run"]({
-      source = entries,
-      sink = function(e)
-        local id = e:match("%d+")
-        local paths = vim.split(p:absolute(), Path.path.sep)
-        local filename = paths[#paths]
-        local job_annotate = Job:new({ command = "task", args = { "annotate", id, "zk: " .. filename } })
-        job_annotate:sync()
-      end,
-    })
+local function _get_note()
+  local paths = vim.split(Path.new(vim.api.nvim_buf_get_name(0)):absolute(), Path.path.sep)
+  return paths[#paths]
+end
+
+local function link()
+  if not _is_note() then
+    return
   end
+
+  local job = Job:new({
+    command = "task",
+    args = { "status:pending", "export" },
+  })
+  job:sync()
+  local entries = {}
+  for _, value in ipairs(vim.json.decode(vim.fn.join(job:result()))) do
+    -- Use fzf to select task
+    table.insert(entries, "[" .. value.id .. "]" .. " " .. value.description)
+  end
+  vim.fn["fzf#run"]({
+    source = entries,
+    sink = function(e)
+      local id = e:match("%d+")
+      local job_annotate = Job:new({ command = "task", args = { "annotate", id, "zk: " .. _get_note() } })
+      job_annotate:sync()
+    end,
+  })
 end
 
 M = {
