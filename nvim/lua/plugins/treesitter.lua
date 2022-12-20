fc221316e5837c183b37f7f06fc09083e9f5254a
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-12-20 13:55:40 +0900

    nvim: Increase treesitter max_filesize

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ require("nvim-treesitter.configs").setup {
     -- the name of the parser)
     -- list of language that will be disabled
     disable = function(_, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
+        local max_filesize = 1024 * 1024 -- 1MiB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
             return true
