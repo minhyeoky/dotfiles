19eeaab91c50fc5b0957b4cdcc2b5974dad4fc56
Minhyeok Lee <minhyeok.lee95@gmail.com>
2023-01-08 17:41:12 +0900

    Don't install all treesitter parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,4 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = "all",
-
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
@@ -28,23 +26,5 @@ require("nvim-treesitter.configs").setup {
   },
   textobjects = {
     enable = true,
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
   },
 }
