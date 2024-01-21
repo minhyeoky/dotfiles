76ca919c745bf081fa67a0ab2e2c1632d439c38b
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-01-22 01:17:25 +0900

    nvim: Unmap `"` (as it is a register name prefix)

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -290,7 +290,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", lsp_map_opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", lsp_map_opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", lsp_map_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", '"', "<cmd>lua vim.lsp.buf.signature_help()<CR>", lsp_map_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", '<C-k>', "<cmd>lua vim.lsp.buf.signature_help()<CR>", lsp_map_opts)
 
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
