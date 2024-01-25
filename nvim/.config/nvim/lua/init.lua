4fa99f51bf082255156a6a9b82bde5d024a5bb50
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-01-25 13:43:57 +0900

    nvim: add nvim-osc52

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -537,3 +537,10 @@ require('gitlinker').setup()
 
 require('oil').setup()
 vim.keymap.set("n", "<leader>fo", "<CMD>lua require('oil').open_float()<CR>", { silent = true, noremap = true })
+
+-------------------------------------------------------------------------------
+-- nvim-osc52
+-------------------------------------------------------------------------------
+vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
+vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
+vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
