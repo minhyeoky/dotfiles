8a5248fa22a1ca6b52978c1df32e9242ea1451ec
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-04-15 00:08:08 +0900

    nvim: Add LazyVim

--- nvim/.config/lazyvim/lua/config/options.lua
+++ nvim/.config/lazyvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+-- exclude asian characters from spell checking
+opt.spelllang = { "en", "cjk" }
