314f768aa4ff5cb65882c3902c036ea2f07519fd
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-05-02 00:31:19 +0900

    alacritty: change font styels

--- nvim/.config/allganize/lua/plugins/lsp.lua
+++ nvim/.config/allganize/lua/plugins/lsp.lua
@@ -10,22 +10,27 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.ruff_lsp.setup({})
-      lspconfig.pyright.setup({})
-
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      lspconfig.pyright.setup({
+        python = {
+          analysis = {
+            diagnosticMode = "workspace",
+          },
+        },
+      })
 
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
           local opts = { buffer = ev.buf }
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
         end,
       })
     end,
