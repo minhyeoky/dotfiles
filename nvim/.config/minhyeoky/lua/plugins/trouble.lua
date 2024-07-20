17e1e2ffc58860b43fd0af18cbe1f3b2d90ba1cc
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-07-20 19:10:56 +0900

    nvim: update trouble.nvim settings

--- nvim/.config/minhyeoky/lua/plugins/trouble.lua
+++ nvim/.config/minhyeoky/lua/plugins/trouble.lua
@@ -1,7 +1,14 @@
 return {
   {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+      auto_preview = false,
+      preview = {
+        type = "float",
+        border = "single",
+        scratch = false,
+      },
+    }, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
       {
@@ -21,7 +28,7 @@ return {
       },
       {
         "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        "<cmd>Trouble lsp toggle focus=true<cr>",
         desc = "LSP Definitions / references / ... (Trouble)",
       },
       {
