0966491ec34559171acef2a6fe6114358749f5b8
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-07-25 13:49:56 +0900

    nvim: add lang/scala.lua

---
+++ nvim/.config/minhyeoky/lua/plugins/lang/scala.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(
+          opts.ensure_installed,
+          {
+            "scala",
+          }
+        )
+      end
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        metals = {},
+      },
+    }
+  },
+}
