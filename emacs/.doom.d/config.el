fed52f789cd70e0fa3775bd4008d7dc2c62bb42f
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-08-02 14:33:37 +0900

    emacs: set org-directory

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -40,7 +40,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/pkm/org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
