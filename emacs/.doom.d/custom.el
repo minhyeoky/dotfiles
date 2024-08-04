7b735c6e68ecf361dcffb0cc70cb0ed53bed924f
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-08-04 13:42:04 +0900

    emacs: improve emacs usability; wrap line visually

--- emacs/.doom.d/custom.el
+++ emacs/.doom.d/custom.el
@@ -11,3 +11,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq-default fill-column 80)
+(add-hook 'visual-line-mode-hook #'visual-fill-column-mode)
+(setq-default visual-fill-column-center-text t)
+(add-to-list 'term-file-aliases '("alacritty" . "xterm"))
