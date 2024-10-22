c0a64868300ba3053afe9cfff56399decd2e56af
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-10-23 02:24:25 +0900

    emacs: fix encoding; set utf-8

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -2,7 +2,21 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
+(setq org-hide-emphasis-markers t)
 
+;; Use UTF-8 encoding
+;; https://stackoverflow.com/questions/2901541/which-coding-system-should-i-use-in-emacs
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system
+  (if (eq system-type 'windows-nt)
+      'utf-16-le  ;; https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
+    'utf-8))
+(prefer-coding-system 'utf-8)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
@@ -142,3 +156,5 @@
 
 ;; Drag-and-drop to `dired`
 (add-hook 'dired-mode-hook 'org-download-enable)
+
+
