183ecb3ccb4d608614eeac69ccb34b57f7dd4b8e
Minhyeok Lee <minhyeok.lee95@gmail.com>
2024-11-09 20:40:47 +0900

    emacs: remove UTF-8 settings

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -4,20 +4,6 @@
 ;; sync' after modifying this file!
 (setq org-hide-emphasis-markers t)
 
-;; Use UTF-8 encoding
-;; https://stackoverflow.com/questions/2901541/which-coding-system-should-i-use-in-emacs
-(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
-(set-language-environment 'utf-8)
-(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
-(setq locale-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-selection-coding-system
-  (if (eq system-type 'windows-nt)
-      'utf-16-le  ;; https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
-    'utf-8))
-(prefer-coding-system 'utf-8)
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 ;; (setq user-full-name "John Doe"
