d09989a6e7ade4615333e5c0db67094eacd08d35
Minhyeok Lee <minhyeok.lee95@gmail.com>
2025-02-16 15:49:52 +0900

    nvim: comment out copilot

--- nvim/.config/minhyeoky/lua/plugins/ai.lua
+++ nvim/.config/minhyeoky/lua/plugins/ai.lua
@@ -1,25 +1,25 @@
 return {
-  {
-    "github/copilot.vim",
-    lazy = false,
-  },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "copilot",
-      auto_suggestions_provider = "copilot",
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-    },
-  },
+  -- {
+  --   "github/copilot.vim",
+  --   lazy = false,
+  -- },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     provider = "copilot",
+  --     auto_suggestions_provider = "copilot",
+  --   },
+  --   build = "make",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --   },
+  -- },
 }
