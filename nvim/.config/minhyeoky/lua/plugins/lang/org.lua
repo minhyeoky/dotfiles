a290d4ec916630f577893d9b09410c0895c62ef2
Minhyeok Lee <minhyeok.lee95@gmail.com>
2025-03-03 12:30:55 +0900

    fix(nvim): remove org from tree-sitter

--- nvim/.config/minhyeoky/lua/plugins/lang/org.lua
+++ nvim/.config/minhyeoky/lua/plugins/lang/org.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- ensure that markdown and markdown_inline are always installed
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "org" })
-      end
-    end,
-  },
-
-  {
     "nvim-orgmode/orgmode",
     event = 'VeryLazy',
     ft = { "org" },
