ca7eb9ab6f6a42f3ae3b4d293944b33e83741512
Minhyeok Lee <minhyeok.lee95@gmail.com>
2025-05-08 16:15:28 +0900

    feat(nvim): add flash.nvim

---
+++ nvim/.config/minhyeoky/lua/plugins/navigation.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      search = {
+        mode = "fuzzy",
+      },
+      modes = {
+        search = {
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
+  },
+}
