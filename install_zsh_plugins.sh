6d61bab2f96ef918714285dea53ae4a52b338b02
Minhyeok Lee <minhyeoklee@Minhyeoks-MacBook-Pro-2.local>
2025-10-19 14:44:18 +0900

    zsh: add zsh plugin setup script

---
+++ install_zsh_plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
