a57d9b99104a31e36283145014a5092c19cc1d52
Minhyeok Lee <minhyeok.lee95@gmail.com>
2025-12-10 07:22:49 +0900

    nvim: change markdown-preview.nvim build command to yarn-based one

--- nvim/.config/nvim/lua/plugins/lang/markdown.lua
+++ nvim/.config/nvim/lua/plugins/lang/markdown.lua
@@ -11,15 +11,11 @@ return {
 
   {
     "iamcco/markdown-preview.nvim",
-    cmd = {
-      "MarkdownPreviewToggle",
-      "MarkdownPreview",
-      "MarkdownPreviewStop",
-    },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
     init = function()
-      vim.g.mkdp_auto_close = 0  -- don't close the preview window when the source buffer is closed
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
   },
 }
