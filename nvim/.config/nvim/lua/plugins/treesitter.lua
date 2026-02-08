b8e1ea1fb12d16502d341126fa1ee00f932a67f9
Minhyeok Lee <minhyeok.lee95@gmail.com>
2026-02-08 19:30:21 +0900

    nvim: add nvim-treesitter-textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
+  -- nvim-treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     version = false,
     build = ":TSUpdate",
     opts = {
-      -- ensure_installed = "all",
       highlight = { enable = true },
       indent = { enable = true },
       folds = { enable = true },
@@ -22,6 +22,64 @@ return {
           vim.opt_local.foldlevel = 2
         end,
       })
+    end,
+  },
+
+  -- nvim-treesitter-textobjects (new API)
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = { lookahead = true },
+        move = { set_jumps = true },
+      })
+
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+      local swap = require("nvim-treesitter-textobjects.swap")
+      local ts = "textobjects"
+
+      -- Select
+      local select_maps = {
+        ["af"] = "@function.outer", ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",    ["ic"] = "@class.inner",
+        ["aa"] = "@parameter.outer", ["ia"] = "@parameter.inner",
+        ["al"] = "@loop.outer",     ["il"] = "@loop.inner",
+        ["ai"] = "@conditional.outer", ["ii"] = "@conditional.inner",
+        ["a="] = "@assignment.outer", ["i="] = "@assignment.rhs",
+      }
+      for key, query in pairs(select_maps) do
+        vim.keymap.set({ "x", "o" }, key, function()
+          select.select_textobject(query, ts)
+        end)
+      end
+
+      -- Move
+      local move_maps = {
+        { "]m", "goto_next_start", "@function.outer" },
+        { "]]", "goto_next_start", "@class.outer" },
+        { "]M", "goto_next_end", "@function.outer" },
+        { "][", "goto_next_end", "@class.outer" },
+        { "[m", "goto_previous_start", "@function.outer" },
+        { "[[", "goto_previous_start", "@class.outer" },
+        { "[M", "goto_previous_end", "@function.outer" },
+        { "[]", "goto_previous_end", "@class.outer" },
+      }
+      for _, m in ipairs(move_maps) do
+        vim.keymap.set({ "n", "x", "o" }, m[1], function()
+          move[m[2]](m[3], ts)
+        end)
+      end
+
+      -- Swap
+      vim.keymap.set("n", "<leader>a", function()
+        swap.swap_next("@parameter.inner")
+      end)
+      vim.keymap.set("n", "<leader>A", function()
+        swap.swap_previous("@parameter.inner")
+      end)
     end,
   },
 }
