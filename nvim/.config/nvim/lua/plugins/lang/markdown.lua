60ae556b7e047803a32302b7829c8067efb50b25
Minhyeok Lee <12870549+minhyeoky@users.noreply.github.com>
2026-04-27 20:45:18 +0900

    nvim: add markview.nvim for in-buffer markdown rendering (#9)

--- nvim/.config/nvim/lua/plugins/lang/markdown.lua
+++ nvim/.config/nvim/lua/plugins/lang/markdown.lua
@@ -8,4 +8,12 @@ return {
     end,
     ft = { "markdown" },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
